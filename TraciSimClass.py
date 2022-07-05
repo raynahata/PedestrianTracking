a5fc5be2cd077d8a47bbbd0be5642b568707cc91
rhata <rhata23@colby.edu>
2022-07-05 07:20:30 -0400

    distance outputs

--- TraciSimClass.py
+++ TraciSimClass.py
@@ -6,7 +6,11 @@ import optparse
 import csv 
 import random
 import math
+from scipy.spatial import distance
 from person import Person
+
+from sumolib import checkBinary 
+import traci 
 
 
 #importing some python modules from the SUMO HOME tools directory 
@@ -16,8 +20,7 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
-from sumolib import checkBinary 
-import traci 
+
 
 def get_options():
     opt_parser=optparse.OptionParser()
@@ -27,41 +30,59 @@ def get_options():
        
 def run():
     step=0 #keep track of the current step
+    dist1=0
+    dist2=0
 
+    #pedestrain parameter arrays for plotting 
+    ped_x=[]
+    ped_y=[]
+    ped_angle=[]
+    ped_speed=[]
+    ped_laneID=[]
+    distance_one=[]
+    distance_two=[]
+    device_one_coord=[2284.5986,968.5631431]
+    device_two_coord=[2293.396508,967.8745266]
 
-
-    pedPos=[]
-    pedAngle=[]
-    pedSpeed=[]
-    pedLaneID=[]
-    posLaneList=[]
 
     while traci.simulation.getMinExpectedNumber()>0: #when we have exahused all of our route files 
         traci.simulationStep() #advance the simulation one timestep 
         ped=traci.person.getIDList()
-        detector=traci.multientryexit.getIDList()
-
         ped1=Person()
+        ped1.scenario=1
+        
+        ped_x.append(ped1.xPos)
+        ped_y.append(ped1.yPos)
+        ped_angle.append(ped1.angle)
+        ped_speed.append(ped1.speed)
+        ped_laneID.append(ped1.laneID)
+        distance_one.append(dist1)
+        distance_two.append(dist2)
 
-        print(ped1.xPos,ped1.yPos)
+        
 
+        #CSV creater 
+        with open('pedestrianInfo.csv', 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(["PositionX","PositionY","Angle","Speed","Current Lane","dist 1","dist 2"])
+            for x in range (step):
+                writer.writerow([ped_x[x],ped_y[x],ped_angle[x],ped_speed[x],ped_laneID[x],distance_one[x+1],distance_two[x+1]])
         
         if(len(ped)!=0):
             ped1.setNewSpeed()
-        
             detectedPerson=traci.multientryexit.getLastStepVehicleIDs('e3_0')
-            #print(len(detectedPerson))
+            ped1.setNewAngle()
+            new_angle=ped1.getAngle()
+        
             #move the person once they reach the entry detector
             if(len(detectedPerson)==1):
-
-
-                ped1.setNewAngle()
-                new_angle=ped1.getAngle()
                 ped1.setNewX(new_angle,ped1.getSpeed())
                 ped1.setNewY(new_angle,ped1.getSpeed())
                 new_X=ped1.getPosX()
                 new_Y=ped1.getPosY()
                 ped1.movePerson(new_X,new_Y,new_angle)
+                dist1=ped1.getDistance(device_one_coord)
+                dist2=ped1.getDistance(device_two_coord)
                 
              
         step+=1 #increment the step
--- person.py
+++ person.py
@@ -6,7 +6,8 @@ import sys
 import optparse
 import random
 import math
-
+from turtle import pos
+from scipy.spatial import distance
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -18,48 +19,73 @@ from sumolib import checkBinary
 import traci 
 
 class Person:
-    def __init__(self):
+    def __init__(self, default_speed = 0.3, angle_bounds = [-1,0]):
         self.ID='p_0'
+        self.default_speed = default_speed
+        self.angle_bounds = angle_bounds
+        self.pos=traci.person.getPosition(self.ID)
         self.xPos=traci.person.getPosition(self.ID)[0]
         self.yPos=traci.person.getPosition(self.ID)[1]
         self.angle=traci.person.getAngle(self.ID)
         self.speed=traci.person.getSpeed(self.ID)
         self.laneID=traci.person.getLaneID(self.ID)
+        self.scenario=0
+        '''
+        0= person walking straight no off movement 
+        1= person drifts up 
+        2=person drifts down
+        
+        
+        '''
 
+    def getPos(self):
+        return self.pos
 
+    
     def getSpeed(self):
         return self.speed
     
     def setNewSpeed(self):
-        self.speed=random.uniform(0,1.3)
+        #self.speed=random.uniform(0,1)
+        self.speed = self.default_speed
         traci.person.setSpeed(self.ID,self.speed)
 
     def getAngle(self):
         return self.angle
     
     def setNewAngle(self):
-        self.angle+=random.uniform(-5,5)
+        self.angle+=random.uniform(self.angle_bounds[0],self.angle_bounds[1])
     
     def getPosX(self):
         return self.xPos
     
     def setNewX(self,newAngle,newSpeed):
-        print(newAngle)
-        print(newSpeed)
+        
         self.xPos+=abs((math.cos(newAngle)*newSpeed))
-       
+        
+        
     
     def getPosY(self):
         return self.yPos
     
     def setNewY(self,newAngle,newSpeed):
-        self.yPos+=abs((math.cos(newAngle)*newSpeed))
+        
+        if self.scenario==0:
+            self.yPos+=abs(math.cos(newAngle)*newSpeed)
+        elif self.scenario==1:
+            self.yPos-=abs(math.cos(newAngle)*newSpeed)
+        
+            
     
     def movePerson(self,x,y,a):
-          traci.person.moveToXY(self.ID,self.laneID , x, y, angle=a, keepRoute=0, matchThreshold=100)
+          traci.person.moveToXY(self.ID,self.laneID , x, y, angle=a, keepRoute=2, matchThreshold=100)
+          self.xPos=x
+          self.yPos=y
+          self.pos=(x,y)
 
 
-        
+    def getDistance(self,device1):
+        dist=distance.euclidean(self.pos,device1)
+        return dist
 
-        
 
