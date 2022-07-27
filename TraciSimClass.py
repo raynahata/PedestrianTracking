3086188174b3979b6acf34031d2a790170431731
rhata <rhata23@colby.edu>
2022-07-26 20:34:22 -0400

    coordinate prediction

--- TraciSimClass.py
+++ TraciSimClass.py
@@ -1,14 +1,15 @@
 from contextlib import nullcontext
 import os
 from pprint import PrettyPrinter
+from ssl import PEM_HEADER
 import sys
 import optparse
 import csv 
 import random
 import math
+from tracemalloc import start
 from scipy.spatial import distance
 from person import Person
-
 from sumolib import checkBinary 
 import traci 
 
@@ -33,94 +34,170 @@ def addNoise(distance,interLength):
     else:
         delta=random.uniform(0,0.05)
         newDist=distance+delta
-    return newDist
-
+    return newDist,delta
 
 def get_options():
     opt_parser=optparse.OptionParser()
     opt_parser.add_option("--nogui",action="store_true",default=False,help="run the commandline version of sumo")
     options, args = opt_parser.parse_args()
     return options
-       
+
 def run():
-    step=0 #keep track of the current step
+
+    step=0
     dist1=0
     dist2=0
+    signalDists=0
+    distOnePrime=0
+    distTwoPrime=0
+    delta_one=0
+    delta_two=0
+    angle1=0
+    angle2=0
+    x1Prime=0
+    y1Prime=0
+    x2Prime=0
+    y2Prime=0
 
-    #pedestrain parameter arrays for plotting 
+    #timestep 
+    time=[]
+
+    #pedestrain ground truth parameters
     ped_x=[]
     ped_y=[]
-    ped_angle=[]
     ped_speed=[]
-    ped_laneID=[]
-    distance_one=[]
-    distance_two=[]
+    ped_lane_ID=[]
+
+    #ground truth euclidian distances 
+    signalDist=[] #between the two singals
+    A1_to_P=[] #from anchor one 
+    A2_to_P=[] #from anchor two 
+
+    #distances with noise added to them
+    distance_one_prime=[]
+    distance_two_prime=[]
+
+    #delta 
+    noise_one=[]
+    noise_two=[]
+
+    #angle predicted using d primes 
+    angle_predict_one=[] #from anchor one 
+    angle_predict_two=[] #from anhor two 
+
+    #pedestrian coordinate calcuated using distances with noise,anchor one 
+    xPosPrime1=[]
+    yPosPrime1=[]
+
+    #pedestrian coordiante calculated using distnace with noise, anchor 2
+    xPosPrime2=[]
+    yPosPrime2=[]
+
+    #coordinates of the two anchors 
     device_one_coord=[2284.5986,968.5631431]
     device_two_coord=[2293.396508,967.8745266]
 
 
+
     while traci.simulation.getMinExpectedNumber()>0: #when we have exahused all of our route files 
         traci.simulationStep() #advance the simulation one timestep 
+        current_sim_time=traci.simulation.getTime()
         ped=traci.person.getIDList()
-        ped1=Person(0)
-      
-        
-        ped_x.append(ped1.xPos)
-        ped_y.append(ped1.yPos)
-        ped_angle.append(ped1.angle)
-        ped_speed.append(ped1.speed)
-        ped_laneID.append(ped1.laneID)
-        distance_one.append(dist1)
-        distance_two.append(dist2)
+        ped1=Person(0) #create the person to access person class
+        detectedPerson=traci.multientryexit.getLastStepVehicleIDs('e3_0')
+        ped1.setNewSpeed()
+        count=0
+              
+        #move the person once they reach the entry detector
+        if(len(detectedPerson)==1):
+            
+            
+            
 
-        
+            #general parameters 
+            xPos=ped1.getPosX()
+            yPos=ped1.getPosY()
+            speed=ped1.getSpeed()
+            
 
-        #CSV creater 
-        with open('pedestrianInfo.csv', 'w', encoding='UTF8') as f:
-            writer = csv.writer(f)
-            writer.writerow(["PositionX","PositionY","Angle","Speed","Current Lane","dist 1","dist 2"])
-            for x in range (step):
-                writer.writerow([ped_x[x],ped_y[x],ped_angle[x],ped_speed[x],ped_laneID[x],distance_one[x+1],distance_two[x+1]])
-        
-        if(len(ped)!=0):
-            ped1.setNewSpeed()
-            detectedPerson=traci.multientryexit.getLastStepVehicleIDs('e3_0')
+            #getting the distance between the person and anchors
+            dist1=ped1.getDistance(device_one_coord) 
+            dist2=ped1.getDistance(device_two_coord)
+            signalDists=distance.euclidean(device_one_coord,device_two_coord)
+
+            
+
+            #adding noise to distances 
+            newDist1tuple=addNoise(dist1,signalDists) #distance from original signal 
+            distOnePrime=newDist1tuple[0]
+            print(distOnePrime)
+            delta_one=newDist1tuple[1]
+            newDist2tuple=addNoise(dist2,signalDists) #distance from singal you are advancing to 
+            distTwoPrime=newDist2tuple[0]
+            delta_two=newDist2tuple[1]
+            
+            
+
+            #new coordinate calculations 
+            #prediction from anchor 1 to ped 
+            angle1=ped1.predictAngle(signalDists,distOnePrime,distTwoPrime)
+            angleRad1=math.radians(90-angle1)
+            x1Prime=device_one_coord[0]+(distOnePrime*math.cos(angleRad1))
+            y1Prime=device_one_coord[1]+(distOnePrime*math.cos(angleRad1))
+            
+            #prediction from anchor 2 to ped 
+            angle2=ped1.predictAngle(signalDists,distTwoPrime,distOnePrime)
+            angleRad2=math.radians(90-angle2)
+            x2Prime=device_two_coord[0]+(distTwoPrime*math.cos(angleRad2))
+            y2Prime=device_two_coord[1]+(distTwoPrime*math.cos(angleRad2))
+            
+            
+
+            #move the person 
             ped1.setNewAngle()
             new_angle=ped1.getAngle()
+            ped1.setNewX(new_angle,ped1.getSpeed())
+            ped1.setNewY(new_angle,ped1.getSpeed())
+            new_X=ped1.getPosX()
+            new_Y=ped1.getPosY()
+            ped1.movePerson(new_X,new_Y,new_angle)
+       
+
+        time.append(current_sim_time)
+        ped_x.append(ped1.xPos)
+        ped_y.append(ped1.yPos)
+        ped_lane_ID.append(ped1.laneID)
+        ped_speed.append(ped1.speed)
+        A1_to_P.append(dist1)
+        A2_to_P.append(dist2)
+        signalDist.append(signalDists)
+        distance_one_prime.append(distOnePrime)
+        distance_two_prime.append(distTwoPrime)
+        noise_one.append(delta_one)
+        noise_two.append(delta_two)
+        angle_predict_one.append(angle1)
+        angle_predict_two.append(angle2)
+        xPosPrime1.append(x1Prime)
+        yPosPrime1.append(y1Prime)
+        xPosPrime2.append(x2Prime)
+        yPosPrime2.append(y2Prime)
+            
+         #write in the CSV 
+        with open('pedestrianInfo.csv', 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(["timestep","PositionX","PositionY","Speed","Lane ID","signalDistance","A1 to P",
+            "A2 to P","Delta 1","Delta two","Distance One Prime","Distance 2 Prime","Angle Predict One","Angle Predict Two",
+            "X Pos One Prime","Y Pos One Prime", "X Pos 2 Prime","Y Pos 2 Prime"])
         
-            #move the person once they reach the entry detector
-            if(len(detectedPerson)==1):
-                ped1.setNewX(new_angle,ped1.getSpeed())
-                ped1.setNewY(new_angle,ped1.getSpeed())
-                new_X=ped1.getPosX()
-                new_Y=ped1.getPosY()
-                ped1.movePerson(new_X,new_Y,new_angle)
-                
-                # ground truth euclidian distances between the signals and the pedestrian 
-                # method located in pedestrian class 
-                dist1=ped1.getDistance(device_one_coord)
-                dist2=ped1.getDistance(device_two_coord)
-                
-                #hardcoded length of the intersection 
-                length=6.4
-
-                #adding noise to the distance calculations in order to mimic noise 
-                newDist1=addNoise(dist1,length)
-                newDist2=addNoise(dist2,length)
-
-                signalDist=distance.euclidean(device_one_coord,device_two_coord)
-
-                angle=ped1.predictAngle(signalDist,newDist1,newDist2)
-                ped1.turnPedAround(angle)
-                print(angle)
-                
-             
+            for x in range (step):
+                writer.writerow([time[x],ped_x[x],ped_y[x],ped_speed[x],ped_lane_ID[x],signalDist[x],A1_to_P[x],A2_to_P[x],
+                    noise_one[x],noise_two[x],distance_one_prime[x],distance_one_prime[x], angle_predict_one[x],angle_predict_two[x],
+                    xPosPrime1[x],yPosPrime1[x],xPosPrime2[x],yPosPrime2[x]])  
         step+=1 #increment the step
-
+    
     traci.close()
     sys.stdout.flush()
-
-
+    
 
 #main loop
 if __name__== "__main__":
@@ -135,4 +212,7 @@ if __name__== "__main__":
     traci.start([sumoBinary, "-c","map2.sumo.cfg","--tripinfo-output","tripinfo.xml"],1)
     run()
 
-    
+
+
+            
+
--- person.py
+++ person.py
@@ -6,8 +6,10 @@ import sys
 import optparse
 import random
 import math
+from this import s
 from turtle import pos
 from scipy.spatial import distance
+import numpy as np
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -19,7 +21,7 @@ from sumolib import checkBinary
 import traci 
 
 class Person:
-    def __init__(self, scenario,default_speed = 0.3, angle_bounds = [-1,0]):
+    def __init__(self, scenario,default_speed = 0.3, angle_bounds = [-2,-1]):
         self.ID='p_0'
         self.default_speed = default_speed
         self.angle_bounds = angle_bounds
@@ -49,7 +51,6 @@ class Person:
         return self.speed
     
     def setNewSpeed(self):
-        #self.speed=random.uniform(0,1)
         self.speed = self.default_speed
         traci.person.setSpeed(self.ID,self.speed)
 
@@ -59,6 +60,9 @@ class Person:
     def setNewAngle(self):
         self.angle+=random.uniform(self.angle_bounds[0],self.angle_bounds[1])
     
+    def setCorrectedAngle(self,angle):
+        self.angle=angle
+
     def getPosX(self):
         return self.xPos
     
@@ -74,12 +78,13 @@ class Person:
     def setNewY(self,newAngle,newSpeed):
         
         if self.scenario==0:
-            self.yPos+=abs(math.cos(newAngle)*newSpeed)
+            newRad=math.radians(newAngle)
+            self.yPos+=(math.cos(newRad)*newSpeed) 
         elif self.scenario==1:
-            self.yPos-=abs(math.cos(newAngle)*newSpeed)
+            newRad=math.radians(newAngle)
+            self.yPos-=math.cos(newRad)*newSpeed 
        
-          
-        
+
             
     
     def movePerson(self,x,y,a):
@@ -99,6 +104,9 @@ class Person:
             dist 1 is the distance between singals (c)
             dist 2 is the distance between pedestrian and the signal they are leaving from (a)
             dist 3 is the distance between pedestrian and the signal they are going towards (b)
+
+            maybe wrong??
+       
         '''
         dist1sq=dist1**2
         dist2sq=dist2**2
@@ -112,7 +120,5 @@ class Person:
         
         return angle
 
-    def turnPedAround(self,angle):
-        if(angle>10):
-            print("pedestrain is heading in the wrong angle")
+        
         
